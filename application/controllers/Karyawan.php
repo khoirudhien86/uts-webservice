9b65b9085d56a1d9bc25a977863b15ca982c720a
khoirudhien86 <khoirudhien86.2@gmail.com>
2021-12-17 22:57:29 +0700

    menambahkan jwt, caching, & idempotensi

--- application/controllers/Karyawan.php
+++ application/controllers/Karyawan.php
@@ -1,47 +1,105 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 require APPPATH . '/libraries/REST_Controller.php';
+require "vendor\autoload.php";
 
 use Restserver\Libraries\REST_Controller;
+use \Firebase\JWT\JWT;
 
 class Karyawan extends REST_Controller
 {
     function __construct($config = 'rest')
     {
         parent::__construct($config);
+        $this->load->driver('cache', array('adapter' => 'apc', 'backup' => 'file'));
     }
 
     //menampilkan data
     public function index_get()
     {
-        $id = $this->get('id');
-        $karyawan = [];
-        if ($id == '') {
-            $data = $this->db->get('karyawan')->result();
-            foreach ($data as $row => $key) :
-                $karyawan[] = [
-                    "id" => $key->id,
-                    "nama" => $key->nama,
-                    "nik" => $key->nik,
-                    "alamat" => $key->alamat,
-                    "_links" => [(object)[
-                        "href" => "keterangan\{$key->id_keterangan}",
-                        "rel" => "keterangan",
-                        "type" => "GET"
-                    ]]
+        $authHeader = $this->input->get_request_header('Authorization');
+        $arr = explode(" ", $authHeader);
+        $jwt = isset($arr[1]) ? $arr[1] : "";
+        $secretkey = base64_encode("gampang");
+
+        if ($jwt) {
+            $decoded = JWT::decode($jwt, $secretkey, array('HS256'));
+
+            $id = $this->get('id');
+            $karyawan = [];
+            if ($id == '') {
+                $data = $this->db->get('karyawan')->result();
+                foreach ($data as $row => $key) :
+                    $karyawan[] = [
+                        "id" => $key->id,
+                        "nama" => $key->nama,
+                        "nik" => $key->nik,
+                        "alamat" => $key->alamat,
+                        "_links" => [(object)[
+                            "href" => "keterangan\{$key->id_keterangan}",
+                            "rel" => "keterangan",
+                            "type" => "GET"
+                        ]]
+                    ];
+                endforeach;
+                $result = [
+                    "took" => $_SERVER["REQUEST_TIME_FLOAT"],
+                    "code" => 200,
+                    "message" => "Response successfully",
+                    "data" => $karyawan,
                 ];
-            endforeach;
+                $this->response($result, 200);
+            } else {
+                if (!is_numeric($id)) {
+                    $result = [
+                        "took" => $_SERVER["REQUEST_TIME_FLOAT"],
+                        "code" => 401,
+                        "message" => "Data Yang Anda Cari Tidak Ditemukan!",
+                        "data" => null,
+                    ];
+                } else {
+                    $this->db->where('id', $id);
+                    $data = $this->db->get('karyawan')->result();
+                    foreach ($data as $row => $key) :
+                        $karyawan[] = [
+                            "id" => $key->id,
+                            "nama" => $key->nama,
+                            "nik" => $key->nik,
+                            "alamat" => $key->alamat,
+                            "_links" => [(object)[
+                                "href" => "keterangan\{$key->id_keterangan}",
+                                "rel" => "keterangan",
+                                "type" => "GET"
+                            ]]
+                        ];
+                    endforeach;
+
+                    $etag = hash('sha256', $key->last_update);
+                    $this->cache->save($etag, $karyawan, 300);
+                    $this->output->set_header('Etag:' . $etag);
+                    $this->output->set_header('Cache-Control: must-revalidate');
+                    if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && $_SERVER['HTTP_IF_NONE_MATCH'] == $etag) {
+                        $this->output->set_header('HTTP/1.1 304 Not Modified');
+                    } else {
+                        $result = [
+                            "took" => $_SERVER["REQUEST_TIME_FLOAT"],
+                            "code" => 200,
+                            "message" => "Response successfully",
+                            "data" => $karyawan,
+                        ];
+                    }
+                }
+                $this->response($result, 200);
+            }
         } else {
-            $this->db->where('id', $id);
-            $karyawan = $this->db->get('karyawan')->result();
+            $result = [
+                "took" => $_SERVER["REQUEST_TIME_FLOAT"],
+                "code" => 401,
+                "message" => "access denided",
+                "token" => null
+            ];
+            $this->response($result, 401);
         }
-        $result = [
-            "took" => $_SERVER["REQUEST_TIME_FLOAT"],
-            "code" => 200,
-            "message" => "Response successfully",
-            "data" => $karyawan,
-        ];
-        $this->response($result, 200);
     }
 
     //menambahkan data
@@ -54,23 +112,36 @@ class Karyawan extends REST_Controller
             'alamat' => $this->post('alamat'),
             'id_keterangan' => $this->post('id_keterangan')
         );
-        $insert = $this->db->insert('karyawan', $data);
-        if ($insert) {
-            $result = [
-                "took" => $_SERVER["REQUEST_TIME_FLOAT"],
-                "code" => 201,
-                "message" => "Data berhasil ditambahkan",
-                "data" => $data
-            ];
-            $this->response($result, 201);
+        $this->db->where("id", $this->post('id'));
+        $this->db->where("nik", $this->post('nik'));
+        $cek = $this->db->get('karyawan')->num_rows();
+        if ($cek == 0) {
+            $insert = $this->db->insert('karyawan', $data);
+            if ($insert) {
+                $result = [
+                    "took" => $_SERVER["REQUEST_TIME_FLOAT"],
+                    "code" => 201,
+                    "message" => "Data berhasil ditambahkan",
+                    "data" => $data
+                ];
+                $this->response($result, 201);
+            } else {
+                $result = [
+                    "took" => $_SERVER["REQUEST_TIME_FLOAT"],
+                    "code" => 502,
+                    "message" => "gagal menambahkan data",
+                    "data" => null
+                ];
+                $this->response($result, 502);
+            }
         } else {
             $result = [
                 "took" => $_SERVER["REQUEST_TIME_FLOAT"],
-                "code" => 502,
-                "message" => "gagal menambahkan data",
-                "data" => null
+                "code" => 304,
+                "message" => "Data yang anda tambahkan sudah ada!",
+                "data" => $data
             ];
-            $this->response($result, 502);
+            $this->response($result, 304);
         }
     }
 
---
+++ application/controllers/login.php
@@ -0,0 +1,69 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+require APPPATH . '/libraries/REST_Controller.php';
+require "vendor\autoload.php";
+
+use Restserver\Libraries\REST_Controller;
+use \Firebase\JWT\JWT;
+
+class Login extends REST_Controller
+{
+    function __construct($config = 'rest')
+    {
+        parent::__construct($config);
+    }
+
+    public function index_post()
+    {
+        $this->db->where('email', $this->post('email'));
+        $data = $this->db->get('users')->result();
+        if (isset($data[0])) {
+            if (password_verify($this->post('password'), $data[0]->password)) {
+                $secret_key = base64_encode("gampang");
+                $issuer_claim = "web service karyawan";
+                $audience_claim = "Mohammad Khoirudin";
+                $issuedat_claim = time();
+                $notbefore_claim = $issuedat_claim + 10;
+                $expire_claim = $issuedat_claim + 86400;
+                $token = array(
+                    "iss" => $issuer_claim,
+                    "aud" => $audience_claim,
+                    "iat" => $issuedat_claim,
+                    "nbf" => $notbefore_claim,
+                    "exp" => $expire_claim,
+                    "data" => array(
+                        "id" => $data[0]->id,
+                        "nama_depan" => $data[0]->nama_depan,
+                        "nama_belakang" => $data[0]->nama_belakang,
+                        "email" => $data[0]->email
+                    )
+                );
+                $jwt = JWT::encode($token, $secret_key);
+                $result = [
+                    "took" => $_SERVER["REQUEST_TIME_FLOAT"],
+                    "code" => 200,
+                    "message" => "Login successfully",
+                    "token" => $jwt
+                ];
+                $this->response($result, 200);
+            } else {
+                $result = [
+                    "took" => $_SERVER["REQUEST_TIME_FLOAT"],
+                    "code" => 401,
+                    "message" => "invalid password, login failed",
+                    "token" => null
+                ];
+                $this->response($result, 401);
+            }
+        } else {
+            $result = [
+                "took" => $_SERVER["REQUEST_TIME_FLOAT"],
+                "code" => 401,
+                "message" => "invalid email, login failed",
+                "token" => null
+            ];
+            $this->response($result, 401);
+        }
+    }
+}
